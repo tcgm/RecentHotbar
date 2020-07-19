3a0536282cce9882d0bd9da6b6f7e1b180014a69
TechFish3000 <42089115+TechFish3000@users.noreply.github.com>
2020-07-19 14:28:15 +0930

    Add files via upload

---
+++ control.lua
@@ -0,0 +1,33 @@
+function scroll(event)
+	--log("Scroll Called")
+	-- this is the main function that handles the scrolling
+	--when the player selects an item
+	
+	-- check if it's already in the hotbar at all (Not Done, will do soon)
+	-- check if it's already first the hotbar (Done)
+	-- check if there is spaces (Not Done, will do soon)
+	-- check if it's an item and the player has enabled that (Done)
+	-- otherwise move everything and add (Done)
+	
+	local player = game.players[event.player_index]
+	if player.cursor_stack.valid_for_read then
+		log(player.cursor_stack.name)
+		log(player.cursor_stack.count)
+		local do_items = settings.get_player_settings(player)["do-items-in-hotbar"].value
+
+		if player.get_quick_bar_slot(1) == player.cursor_stack.prototype or (player.cursor_stack.prototype.place_result == nil and do_items == false) then
+		do end
+		
+		else
+			for num=0,8 do
+				player.set_quick_bar_slot(10 - num, player.get_quick_bar_slot(9 - num))
+			end
+			player.set_quick_bar_slot(1, player.cursor_stack.name)
+		end
+	end
+
+	
+	
+end
+
+script.on_event(defines.events.on_player_cursor_stack_changed, scroll)
---
+++ settings.lua
@@ -0,0 +1,9 @@
+data:extend({
+
+    {
+        type = "bool-setting",
+        name = "do-items-in-hotbar",
+        setting_type = "runtime-per-user",
+        default_value = false
+    }
+})
